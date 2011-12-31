378c9c16fb9ed53e45e114fd3d63858ea0a569b3
須釜 鶏太 <sugama@jamadam.com>
2011-12-31 16:20:15 +0900

    improved mail title again

--- lib/MojoDownMonitor.pm
+++ lib/MojoDownMonitor.pm
@@ -82,7 +82,7 @@ our $VERSION = '0.01';
                     
                     my @change_msg_tbl = ();
                     $change_msg_tbl[0][1] = 'detected an error';
-                    $change_msg_tbl[1][0] = 'detected resolved';
+                    $change_msg_tbl[1][0] = 'detected an error resolved';
                     $change_msg_tbl[1][1] = 'detected an error continuously';
                     my $title = $change_msg_tbl[$new_log->{OK}][$last_log->{OK}];
                     
